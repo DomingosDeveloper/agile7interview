92659a84c7d8d06faf5ec2245e0ad626faf86967
Vinícius Domingos <vinicius.domingos2022@gmail.com>
2024-10-31 10:47:35 -0300

    updt: Models Folder

---
+++ models/book.ts
@@ -0,0 +1,14 @@
+export interface Book {
+    id?: number;
+    
+    title: string;
+    
+    author: string;
+    
+    publishedYear: string;
+    
+    subject: string;
+    
+    quantity: number;
+    
+}
